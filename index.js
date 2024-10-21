2cb2856d94302b644c7b5b294c8b1547e786d692
Saksham Kulshrestha <sakshamkul224@gmail.com>
2024-10-21 22:37:21 +0530

    Initialized Tailwind css

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname,'public')));
+app.use('/public',express.static(path.join(__dirname,'/public')));
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./views/**/*.{html,js,ejs}",
+    "./public/**/*.{html,js}",
+    "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
