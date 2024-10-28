144761f21c7c2bfb4bc9991e78caaf4e3750f20e
Saksham Kulshrestha <sakshamkul224@gmail.com>
2024-10-29 00:04:05 +0530

    add create-quiz route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 8080;
@@ -9,6 +10,7 @@ const __dirname = path.dirname(__filename);
 app.use('/public',express.static(path.join(__dirname,'/public')));
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/home",(req,res)=>{
     res.render('home');
@@ -20,6 +22,11 @@ app.get("/login",(req,res)=>{
 
 app.get("/form",(req,res)=>{
     res.render('form')
+})
+
+app.post("/create-quiz",(req,res)=>{
+    console.log(req.body.topic)
+    res.redirect('home')
 })
 
 app.listen(port,()=>{
